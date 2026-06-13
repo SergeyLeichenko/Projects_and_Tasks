f4b6c67349dd795afae3b56d7d188d2b77432281
SergeyLeichenko <serg88917072@gmail.com>
2026-06-13 18:51:38 +0300

    fix: axios base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://6a2bbcf43e2b60ab038eb99f.mockapi.io/api/',
 })
 
 export default api
